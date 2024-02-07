91487cc450f3f742faa62e962dcef7d0b4c65266
w63402 <78501922+w63402@users.noreply.github.com>
2024-02-07 04:06:38 +0100

    100+ Files

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int melons = 0;
+
+    [SerializeField] private Text melonsText;
+
+    [SerializeField] private AudioSource collectionSoundEfect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Melon"))
+        {
+            collectionSoundEfect.Play();
+            Destroy(collision.gameObject);
+            melons++;
+            melonsText.text = "Melons: " + melons;
+        }
+    }
+}
